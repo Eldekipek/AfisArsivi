1acc06b480406170dbaabf80dc816dabe3ddf1c3
İpek <190290020@firat.edu.tr>
2022-08-19 02:43:00 +0300

    designer page made

--- resources/views/front/detailPages/designers.blade.php
+++ resources/views/front/detailPages/designers.blade.php
@@ -1,7 +1,7 @@
 @extends("front.layout")
 @section("content")
     <div class="container">
-        <div class="designers-input m-3">
+        <div class="designers-input m-5">
         <nav class="navbar navbar-light bg-light">
             <form class="form-inline" style="display: flex">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -11,23 +11,238 @@
     </div>
 
         <div class="profile-list-item">
-            <div class="list-row header-list">
-                <div class="col-12 col-md-6 list-item name">
-                    <button type="button" class="list-toggle"><span class="sort-arrow">Name</span></button>
-                </div>
-                <div class="col-12 col-md-1-5 list-item">
-                    <button type="button" class="list-toggle"><span class="sort-arrow">Country</span></button>
-                </div>
-                <div class="col-12 col-md-1 list-item">
-
-                </div>
-                <div class="col-12 col-md-1 list-item">
-
+            <div class="list-row header-list" style="display: flex">
+                <div class="col-12 col-md-6 list-item name" style="display: flex; gap: 20px;">
+                    <button type="button" class="list-toggle" style="background-color: transparent; border:none;"><span class="sort-arrow">Profile</span></button>
                 </div>
                 <div class="col-12 col-md-2 list-item">
-
+                    <button type="button" class="list-toggle" style="background-color: transparent; border:none;"><span class="sort-arrow">Country</span></button>
+                </div>
+                <div class="col-12 col-md-1 list-item">
+                    <button type="button" class="list-toggle" style="background-color: transparent; border:none;"><span class="sort-arrow">Category</span></button>
+                </div>
+                <div class="col-12 col-md-1 list-item">
+                    <button type="button" class="list-toggle" style="background-color: transparent; border:none;"><span class="sort-arrow">Birth</span></button>
+                </div>
+                <div class="col-12 col-md-2 list-item">
+                    <button type="button" class="list-toggle" style="background-color: transparent; border:none;"><span class="sort-arrow">Website</span></button>
                 </div>
             </div>
         </div>
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex; text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
+
+        <article class="profile-list-item" style="    border-top: 1px solid #0e0e0e;  ">
+            <div class="list-row">
+                <a href="" class="col-12 col-md-6 list-item name" style="display: flex;text-decoration: none;color:black">
+                    <div class="profile-icon large">
+                        <picture>
+                            <img src="https://images.typographicposters.com/icon-2x-p3/_profile/node/62460af2ab4a51c8cc8f1ac5.jpg" alt="">
+                        </picture>
+                    </div>
+                    <h1 class="title">NODE Berlin Oslo</h1>
+                </a>
+
+                <div class="col-12 col-md-2 list-item info">
+                    <h6>Germany &amp; Norway</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>studio</h6>
+                </div>
+                <div class="col-12 col-md-1 list-item info">
+                    <h6>est. 2003</h6>
+                </div>
+                <div class="col-12 col-md-2 list-item info common-links">
+                    <h6><a href="https://node.international/" target="_blank" rel="noopener">node.international</a>
+                    </h6>
+                </div>
+            </div>
+        </article>
     </div>
 @endsection
