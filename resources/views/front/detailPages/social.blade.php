b761a477309101edeb01bfb836e44cd08baa4b24
İpek <190290020@firat.edu.tr>
2022-08-24 15:55:37 +0300

    fixed header img

--- resources/views/front/detailPages/social.blade.php
+++ resources/views/front/detailPages/social.blade.php
@@ -27,6 +27,62 @@
                     <small class="caption lining-numbers">Offset,</small><!--Baskı tekniği-->
                     <small class="caption lining-numbers">1000 x 700</small><!--ebat-->
                 </span>
+                    <div class="afis-buttons">
+                        <div class="icons">
+
+                        </div>
+                        <div class="afis-detail">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn- border" data-toggle="modal" data-target="#exampleModal">
+                                Detay
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Afiş Başlığı</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <picture>
+                                                <img style="width: 250px; height: 250px; margin: auto;display: flex" src="{{asset('https://images.typographicposters.com/search-2x-p3/apeloig/0117738-tnt-affiche-saison-2012-13-rvb-01.jpg')}}" alt="">
+                                            </picture>
+                                            <div class="mt-3" style="    display: flex;
+                                                                         flex-direction: column;
+                                                                         align-items: center;">
+                                        <span class="common-links">
+                                            <h2 class="title"><strong> <!--Tasarımcı adı--->
+                                                <span><a href="/apeloig" class="">İpek Eldek</a></span></strong>
+                                            </h2>
+                                            <h2 class="title"><strong> <!--Proje adı--->
+                                                <span><a href="/apeloig" class="">Studio Philippe Apeloig</a></span></strong>
+                                            </h2><!---->
+                                            <h1 class="title"> 2012-2013”, 2012</h1> <!--Tarih--->
+                                            <small class="caption lining-numbers">France |</small><!--Kullanıldığı yer-->
+                                            <small class="caption lining-numbers">Offset,</small><!--Baskı tekniği-->
+                                            <small class="caption lining-numbers">1000 x 700</small><!--ebat-->
+                                        </span>
+                                                <div class="icons">
+                                                    <div class="like">
+                                                        <i class="fa-solid fa-heart"></i>
+                                                    </div>
+                                                    <div class="add-collection">
+                                                        <i class="fa-solid fa-folder-plus"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
 
                 </div>
             </article>
