730dbe415a323fff9a0941c2c97ef919f38060ed
Fatihozveren <fatihozveren8520@hotmail.com>
2022-08-27 17:46:44 +0300

    tasarımcılar sayfası düzenlendi

--- app/Http/Controllers/front/DesignerController.php
+++ app/Http/Controllers/front/DesignerController.php
@@ -9,7 +9,7 @@ use App\Models\User;
 class DesignerController extends Controller
 {
     public function index(){
-        $designers = User::all();
+        $designers = User::where('id' , '>', 3)->get();
 
 
         return view('front.detailPages.designers', compact('designers'));
