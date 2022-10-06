cc355ddcb6563f6ac6521aa6a13fcfbb3c0fc382
semihycl_1 <190541019@firat.edu.tr>
2022-10-06 10:30:18 +0300

    kullanıcı özgeçmiş alanı eklendi

--- resources/views/front/detailPages/profile.blade.php
+++ resources/views/front/detailPages/profile.blade.php
@@ -27,12 +27,7 @@
              </div>
             <div class="summary">
                 <div class="caticerik">
-                    Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının
-                    bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri
-                    standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek
-                    değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının
-                    yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları
-                    ile popüler olmuştur.
+                    {{$designer->explanation}}
                 </div>
                 <div class="devaminioku">
                     <div class="devamyazi btn" style="font-weight: 600;border-bottom: 1px solid #000000">DEVAMINI OKU</div>
