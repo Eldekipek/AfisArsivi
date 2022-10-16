e34def3c24d05a8572b1c8104a2aaaf9b7060a26
Fatihozveren <fatihozveren8520@hotmail.com>
2022-10-16 13:13:41 +0300

    Contact seeder added

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('contact')->insert([
+            'address' => "Adres",
+            'email' => 'Email',
+            'phone_number' =>'',
+        ]);
+    }
+}
